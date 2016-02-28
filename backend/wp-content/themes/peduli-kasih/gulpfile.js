02bc623e65ebb5ae6c4ad75702c01eac1a7af51f
enoa7 <gamal.ardi@gmail.com>
2016-02-28 10:54:13 +0700

    update gulpfile.js and update foldername

--- backend/wp-content/themes/peduli-kasih/gulpfile.js
+++ backend/wp-content/themes/peduli-kasih/gulpfile.js
@@ -6,33 +6,12 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
 var reload = browserSync.reload;
 // var livereload = require('gulp-livereload');
 
 /* path to peduli kasih theme */
-var path = 'backend/wp-content/themes/peduli-kasih';
-
-gulp.task('sass', function() {
-    gulp.src(path + '/inc/stylesheet/main.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(rename({
-            basename: 'style'
-        }))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path)) //output the file at theme root
-        // .pipe(livereload());
-});
-
-gulp.task('watch', ['sass'], function() {
-    // livereload.listen();
-    gulp.watch(path + '/inc/stylesheet/**/*.scss', ['sass']);
-    gulp.watch(path + '/inc/stylesheet/*.scss', ['sass']);
-});
-
-gulp.task('default', ['sass', 'watch']);
+// var path = 'backend/wp-content/themes/peduli-kasih';
 
 /* ========================================================
  * Tasks with Browser Sync
@@ -47,34 +26,27 @@ gulp.task('browserSync', function() {
 
     browserSync.init(files, {
         proxy: "localhost/pedulikasih/",
-        // middleware: function(req, res, next) {
-        //     console.log('Adding CORS header for ' + req.method + ': ' + req.url);
-        //     res.setHeader('Access-Control-Allow-Origin', '*');
-        //     next();
-        // },
         notify: 'false'
     });
 });
 
-gulp.task('sass_live', function() {
-    return gulp.src('inc/stylesheet/main.scss')
+gulp.task('sass', function() {
+    return gulp.src('./inc/sass/main.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(rename({
-            basename: 'style'
-        }))
+        .pipe(rename({basename: 'style'}))
         .pipe(autoprefixer())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./')) //output the file at root (app/)
         .pipe(reload({ stream: true }));
 });
 
-gulp.task('js_live', function() {
+gulp.task('js', function() {
     return gulp.src([
-            'inc/js/jquery.js',
-            'inc/js/slick.js',
-            'inc/js/main.js',
+            './inc/js/jquery.js',
+            './inc/js/slick.js',
+            './inc/js/main.js',
         ])
         .pipe(concat('main.js'))
         .pipe(gulp.dest('./'))
@@ -82,7 +54,8 @@ gulp.task('js_live', function() {
 });
 
 
-gulp.task('serve', ['sass_live', 'js_live', 'browserSync'], function() {
-    gulp.watch('inc/stylesheet/**/*.scss', ['sass_live']);
-    gulp.watch('inc/js/*.js', ['js_live']);
+gulp.task('default', ['sass', 'js', 'browserSync'], function() {
+    gulp.watch('./inc/sass/**/*.scss', ['sass']);
+    gulp.watch('./inc/sass/*.scss', ['sass']);
+    gulp.watch('./inc/js/*.js', ['js']);
 });
